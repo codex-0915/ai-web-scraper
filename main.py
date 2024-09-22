aa87772f3df5e0757fa05b8683297cd108ebeb66
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2024-09-22 21:44:29 +0800

    Clean DOM content and print content on the web UI

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import *
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a Website URL: ")
@@ -7,5 +7,12 @@ unblock_website = False
 
 if st.button("Scrape Site"):
     st.write("Scraping the website...")
+
     result = scrape_website(url, unblock_website)
-    print(result)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
